6d8bfbd29972d958c19c7163e6e0afeccb84722d
riley <riley@LAPTOP-QBV9V21S>
2024-02-11 17:34:41 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Hill_Mortgage
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrincipal = new System.Windows.Forms.Label();
+            this.lbl_interest = new System.Windows.Forms.Label();
+            this.txtPrincipal = new System.Windows.Forms.TextBox();
+            this.lblYears = new System.Windows.Forms.Label();
+            this.cboInterest = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtOtherYear = new System.Windows.Forms.TextBox();
+            this.radOther = new System.Windows.Forms.RadioButton();
+            this.rad30year = new System.Windows.Forms.RadioButton();
+            this.rad15year = new System.Windows.Forms.RadioButton();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPrincipal
+            // 
+            this.lblPrincipal.AutoSize = true;
+            this.lblPrincipal.Location = new System.Drawing.Point(48, 28);
+            this.lblPrincipal.Name = "lblPrincipal";
+            this.lblPrincipal.Size = new System.Drawing.Size(53, 13);
+            this.lblPrincipal.TabIndex = 0;
+            this.lblPrincipal.Text = "Principal: ";
+            // 
+            // lbl_interest
+            // 
+            this.lbl_interest.AutoSize = true;
+            this.lbl_interest.Location = new System.Drawing.Point(48, 146);
+            this.lbl_interest.Name = "lbl_interest";
+            this.lbl_interest.Size = new System.Drawing.Size(42, 13);
+            this.lbl_interest.TabIndex = 1;
+            this.lbl_interest.Text = "Interest";
+            // 
+            // txtPrincipal
+            // 
+            this.txtPrincipal.Location = new System.Drawing.Point(97, 25);
+            this.txtPrincipal.Name = "txtPrincipal";
+            this.txtPrincipal.Size = new System.Drawing.Size(100, 20);
+            this.txtPrincipal.TabIndex = 6;
+            this.txtPrincipal.TextChanged += new System.EventHandler(this.txtPrincipal_TextChanged);
+            this.txtPrincipal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrincipal_KeyPress);
+            // 
+            // lblYears
+            // 
+            this.lblYears.AutoSize = true;
+            this.lblYears.Location = new System.Drawing.Point(48, 54);
+            this.lblYears.Name = "lblYears";
+            this.lblYears.Size = new System.Drawing.Size(73, 13);
+            this.lblYears.TabIndex = 10;
+            this.lblYears.Text = "Term In Years";
+            // 
+            // cboInterest
+            // 
+            this.cboInterest.FormattingEnabled = true;
+            this.cboInterest.Items.AddRange(new object[] {
+            "3.0",
+            "3.1",
+            "3.2",
+            "3.3",
+            "3.4",
+            "3.5",
+            "3.6",
+            "3.7",
+            "3.8",
+            "3.9",
+            "4.0",
+            "4.1",
+            "4.2",
+            "4.3",
+            "4.4",
+            "4.5"});
+            this.cboInterest.Location = new System.Drawing.Point(89, 143);
+            this.cboInterest.Name = "cboInterest";
+            this.cboInterest.Size = new System.Drawing.Size(38, 21);
+            this.cboInterest.TabIndex = 11;
+            this.cboInterest.SelectedIndexChanged += new System.EventHandler(this.cboInterest_SelectedIndexChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(51, 170);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(156, 36);
+            this.btnCalculate.TabIndex = 12;
+            this.btnCalculate.Text = "Calculate Payment";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(51, 212);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 40);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(132, 212);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 40);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtOtherYear
+            // 
+            this.txtOtherYear.Location = new System.Drawing.Point(109, 115);
+            this.txtOtherYear.Name = "txtOtherYear";
+            this.txtOtherYear.Size = new System.Drawing.Size(88, 20);
+            this.txtOtherYear.TabIndex = 15;
+            this.txtOtherYear.TextChanged += new System.EventHandler(this.txtOtherYear_TextChanged);
+            this.txtOtherYear.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtOtherYear_KeyPress);
+            // 
+            // radOther
+            // 
+            this.radOther.AutoSize = true;
+            this.radOther.Location = new System.Drawing.Point(51, 116);
+            this.radOther.Name = "radOther";
+            this.radOther.Size = new System.Drawing.Size(60, 17);
+            this.radOther.TabIndex = 16;
+            this.radOther.TabStop = true;
+            this.radOther.Text = "Other...";
+            this.radOther.UseVisualStyleBackColor = true;
+            this.radOther.CheckedChanged += new System.EventHandler(this.radOther_CheckedChanged);
+            // 
+            // rad30year
+            // 
+            this.rad30year.AutoSize = true;
+            this.rad30year.Location = new System.Drawing.Point(51, 93);
+            this.rad30year.Name = "rad30year";
+            this.rad30year.Size = new System.Drawing.Size(62, 17);
+            this.rad30year.TabIndex = 17;
+            this.rad30year.TabStop = true;
+            this.rad30year.Text = "30 Year";
+            this.rad30year.UseVisualStyleBackColor = true;
+            this.rad30year.CheckedChanged += new System.EventHandler(this.rad30year_CheckedChanged);
+            // 
+            // rad15year
+            // 
+            this.rad15year.AutoSize = true;
+            this.rad15year.Location = new System.Drawing.Point(51, 70);
+            this.rad15year.Name = "rad15year";
+            this.rad15year.Size = new System.Drawing.Size(62, 17);
+            this.rad15year.TabIndex = 18;
+            this.rad15year.TabStop = true;
+            this.rad15year.Text = "15 Year";
+            this.rad15year.UseVisualStyleBackColor = true;
+            this.rad15year.CheckedChanged += new System.EventHandler(this.rad15year_CheckedChanged);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(55, 268);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(62, 13);
+            this.lblOutput.TabIndex = 19;
+            this.lblOutput.Text = "placeholder";
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 302);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.rad15year);
+            this.Controls.Add(this.rad30year);
+            this.Controls.Add(this.radOther);
+            this.Controls.Add(this.txtOtherYear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.cboInterest);
+            this.Controls.Add(this.lblYears);
+            this.Controls.Add(this.txtPrincipal);
+            this.Controls.Add(this.lbl_interest);
+            this.Controls.Add(this.lblPrincipal);
+            this.Name = "MainFrm";
+            this.Text = "Riley Hill : Mortgage Calculator";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrincipal;
+        private System.Windows.Forms.Label lbl_interest;
+        private System.Windows.Forms.TextBox txtPrincipal;
+        private System.Windows.Forms.Label lblYears;
+        private System.Windows.Forms.ComboBox cboInterest;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtOtherYear;
+        private System.Windows.Forms.RadioButton radOther;
+        private System.Windows.Forms.RadioButton rad30year;
+        private System.Windows.Forms.RadioButton rad15year;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hill_Mortgage
+{
+    public partial class MainFrm : Form
+    {
+        double principal = 0D;
+        int years = 0;
+        double interest = 0D;
+        const int months = 12;
+        double total = 0D;
+        double r = 0D;
+        double n = 0D;
+        double P = 0D;
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            txtOtherYear.Visible = false;
+            lblOutput.Visible = false;
+            rad30year.Checked = true;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void radOther_CheckedChanged(object sender, EventArgs e)
+        {
+            txtOtherYear.Visible = true;
+        }
+
+        private void rad15year_CheckedChanged(object sender, EventArgs e)
+        {
+            txtOtherYear.Visible = false;
+            years = 15;
+        }
+
+        private void rad30year_CheckedChanged(object sender, EventArgs e)
+        {
+            txtOtherYear.Visible = false;
+            years = 30;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            principal = 0D;
+            years = 30;
+            interest = 0D;
+            total = 0D;
+            r = 0D;
+            n = 0D;
+            P = 0D;
+            txtPrincipal.Clear();
+            cboInterest.SelectedItem = null;
+            rad30year.Checked = true;
+            lblOutput.Text = string.Empty;
+            lblOutput.Visible = false;
+        }
+
+        private void cboInterest_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cboInterest.SelectedIndex)
+            {
+                case 0:
+                    interest = 0.03; 
+                    break;
+                case 1:
+                    interest = 0.031;
+                    break;
+                case 2:
+                    interest = 0.032;
+                    break;
+                case 3:
+                    interest = 0.033;
+                    break;
+                case 4:
+                    interest = 0.034;
+                    break;
+                case 5:
+                    interest = 0.035;
+                    break;
+                case 6:
+                    interest = 0.036;
+                    break;
+                case 7:
+                    interest = 0.037;
+                    break;
+                case 8:
+                    interest = 0.038;
+                    break;
+                case 9:
+                    interest = 0.039;
+                    break;
+                case 10:
+                    interest = 0.04;
+                    break;
+                case 11:
+                    interest = 0.041;
+                    break;
+                case 12:
+                    interest = 0.042;
+                    break;
+                case 13:
+                    interest = 0.043;
+                    break;
+                case 14:
+                    interest = 0.044;
+                    break;
+                case 15:
+                    interest = 0.045;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void txtPrincipal_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPrincipal.Text))
+            {
+                principal = double.Parse(txtPrincipal.Text);
+            };
+        }
+
+        private void txtOtherYear_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtOtherYear.Text))
+            {
+                years = int.Parse(txtOtherYear.Text);
+            };
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            r = interest / months;
+            n = years * months;
+            P = principal;
+            total = P * (r * Math.Pow((1 + r), n) / (Math.Pow((1 + r), n) - 1));
+            lblOutput.Visible = true;
+            lblOutput.Text = "Monthly payment is: " + total.ToString("C", CultureInfo.CurrentCulture);
+        }
+
+        private void txtPrincipal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtOtherYear_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
